ed6fdc0fad14bb1b524d04b303af71c945db2442
Mariano Ruz Jurado <60434009+MarianoRuzJurado@users.noreply.github.com>
2025-05-26 12:46:40 +0200

    Add files via upload

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,11 +1,23 @@
+#' Cellbender Python Environment
+#'
+#' A `BasiliskEnvironment` containing Cellbender and Python 3.7, used in the DOtools package for running Cellbender-related functionality.
+#'
+#' @return A [basilisk::BasiliskEnvironment()] object preconfigured with Cellbender.
+#' @export
 #TODO write this function correctly for creating the Cellbender env maybe use that function in cellbender call and if its not there create it
 cellbender_env <- basilisk::BasiliskEnvironment(
   envname = "cellbender",
   pkgname = "DOtools",
   packages = c("python=3.7.16",
                "cellbender=0.3.2")
-  )
+)
 
+#' DOtools Python Environment
+#'
+#' A `BasiliskEnvironment` containing general-purpose Python packages used in DOtools, including celltypist and scanpro, with Python 3.11.
+#'
+#' @return A [basilisk::BasiliskEnvironment()] object preconfigured with DOtools-related packages.
+#' @export
 #TODO write this function correctly for creating the remaining python function env maybe use that function in cellbender call and if its not there create it
 dotools_env <- basilisk::BasiliskEnvironment(
   envname = "dotools",
@@ -13,4 +25,4 @@ dotools_env <- basilisk::BasiliskEnvironment(
   packages = c("python=3.11.11",
                "celltypist=1.6.3",
                "scanpro=0.3.2")
-  )
+)
